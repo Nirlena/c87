9afb5f48faa3df3994d9d8d8ba733accf11eeade
Nirlena <102743997+Nirlena@users.noreply.github.com>
2023-01-30 11:33:59 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAD7OFDWxevdTAG_vvwt00EXlwzm5jLSHk",
+    authDomain: "project-spectagram-55a0f.firebaseapp.com",
+    databaseURL: "https://project-spectagram-55a0f-default-rtdb.firebaseio.com",
+    projectId: "project-spectagram-55a0f",
+    storageBucket: "project-spectagram-55a0f.appspot.com",
+    messagingSenderId: "1036549336796",
+    appId: "1:1036549336796:web:f365176b76872713f44e19"
+  };
